5e8b06713ac5fc0fa78a3ee173d89fbdde6108f2
alealejandro <alex.m.4155@gmail.com>
2016-10-16 21:46:56 -0400

    Clean up output, display nicer

--- diet.rb
+++ diet.rb
@@ -16,14 +16,15 @@ create_table_cmd = <<-SQL
 SQL
 # serving size allow grams, cups, scoops
 
-def create_food(db, category, sub_category, name, style, serving_size, calories, protein, fat, carbs)
-  db.execute("INSERT INTO foods (category, sub_category, name, style, serving_size, calories, protein, fat, carbs) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", [category, sub_category, name, style, serving_size, calories, protein, fat, carbs])
+def create_food(category, sub_category, name, style, serving_size, calories, protein, fat, carbs)
+  DB.execute("INSERT INTO foods (category, sub_category, name, style, serving_size, calories, protein, fat, carbs) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", [category, sub_category, name, style, serving_size, calories, protein, fat, carbs])
 end
 
-# Enter foods into database
-def enter_food(db)
+# Enter new food item into database
+def enter_food
 	done = false
 	until done
+		puts
 		puts "Would you like to add a food item to the foods database? (y/n)"
 		print "\n> "
 		response = gets.chomp
@@ -50,33 +51,44 @@ def enter_food(db)
 			print "Carbs: "
 			carbs = gets.chomp.to_f
 		end
-		create_food(db, category, sub_category, name, style, serving_size, calories, protein, fat, carbs)
+		create_food(category, sub_category, name, style, serving_size, calories, protein, fat, carbs)
 	end
 end
 
-# Check if food name exists in foods table
-def exists_check(db, food_name)
-	db.execute("SELECT 1 FROM foods WHERE name = ?", [food_name]).length > 0
+# Displays names of all foods in foods db
+def display_foods
+	foods_names = DB.execute("SELECT name FROM foods")
+	puts 
+	puts "Foods".center(40, '-')
+	foods_names.each do |food_name|
+		puts "#{food_name[0]}"
+	end
+	puts "-".center(40, '-')
 end
 
-# Enter what you ate (food name, servings)
-def ask_diet(database)
-	db1 = database
+# Check if food name exists in foods table
+def exists_check(food_name)
+	DB.execute("SELECT 1 FROM foods WHERE name = ?", [food_name]).length > 0
+end
+
+# Enter what you ate that day (food name, servings)
+def ask_diet
 	done = false
 	total_nutrition_array = []
 
 	until done
-		puts "\nWhat did you eat today (food name)? Type 'done' to exit"
+		display_foods
+		puts "\nWhat did you eat today? Type 'done' to exit"
 		print "\n> "
 		food_item = gets.chomp.downcase
 
 		if food_item == "done"
 			done = true
 			break
-		elsif exists_check(db1, food_item)
-			print "\nHow many servings? "
+		elsif exists_check(food_item)
+			print "\nServings? "
 			servings = gets.chomp.to_i
-			nutrition = db1.execute("SELECT calories, protein, fat, carbs FROM foods WHERE name = ?", [food_item])
+			nutrition = DB.execute("SELECT calories, protein, fat, carbs FROM foods WHERE name = ?", [food_item])
 			food_value_by_servings = []
 			nutrition.each do |food|
 				food.each do |nutrition_value|
@@ -95,7 +107,7 @@ end
 
 def calc_total(ar)
 	j = 0 
-	# nutritional value 
+	# nutritional value (NV) 
 	new_ar = []
 	while j < 4
 		i = 0
@@ -112,7 +124,7 @@ def calc_total(ar)
 	new_ar
 end
 
-# calculate difference between ingested food vs. daily goals
+# Ingested food vs. daily goals
 def calc_diff(ingested_ar, goals_ar)
 	difference_nutrition = []
 	i = 0
@@ -123,16 +135,16 @@ def calc_diff(ingested_ar, goals_ar)
 	difference_nutrition
 end
 
-# Driver code
+##### Driver code #####
 
-db = SQLite3::Database.new("diet.db")
-db.execute(create_table_cmd)
-enter_food(db)
+DB = SQLite3::Database.new("diet.db")
+DB.execute(create_table_cmd)
+
+enter_food
 
 # Set daily goals (cal, pro, fat, carbs)
 daily_nutrition_goals = [2400, 180, 80, 240]
-
-total_nutrition = calc_total(ask_diet(db))
+total_nutrition = calc_total(ask_diet)
 calculated_difference = calc_diff(total_nutrition, daily_nutrition_goals)
 
 puts "                            [Cal, Protein, Fat, Carbs]"
@@ -141,9 +153,14 @@ puts "Daily nutrition goals:      #{daily_nutrition_goals}"
 puts "Difference:                 #{calculated_difference}"
 
 # Extra features:
-	# If under goal, show how much under & recommend food items that will reach goal
-	# If over goal, show how much over & recommend what to cut out based on that day's diet for the next day
+
+	# RECOMMENDATIONS
+		# If under goal, show how much under & recommend food items that will reach goal
+		# If over goal, show how much over & recommend what to cut out based on that day's diet for the next day
 
 	# Export to excel file?
+
+	# Each food item ingested as a % of daily goals?
+
 	# Machine learning photo recognition
-	# Or if at restaurant, contact restaurant for nutritional values
+		# Or if at restaurant, contact restaurant for nutritional values
